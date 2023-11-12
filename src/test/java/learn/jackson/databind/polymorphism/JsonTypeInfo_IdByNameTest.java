5db924c96a1d2d432ee7748402edf208d20995ed
Krevis <venzersiz@gmail.com>
2023-11-12 21:16:19 +0900

    Polymorphism > @JsonTypeInfo

---
+++ src/test/java/learn/jackson/databind/polymorphism/JsonTypeInfo_IdByNameTest.java
@@ -0,0 +1,65 @@
+package learn.jackson.databind.polymorphism;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+class JsonTypeInfo_IdByNameTest {
+
+    ObjectMapper mapper = new ObjectMapper()
+        .enable(SerializationFeature.INDENT_OUTPUT);
+
+    @Test
+    void idByName() throws JsonProcessingException {
+        Vehicle vehicle = new Car("68오 8269");
+
+        String json = mapper.writeValueAsString(vehicle);
+        assertThat(json).isEqualTo("""
+                                       {
+                                         "@type" : "JsonTypeInfo_IdByNameTest$Car",
+                                         "licensePlate" : "68오 8269"
+                                       }""");
+        // @JsonTypeInfo의 use 요소의 값으로 타입 식별자를 NAME으로 설정했기 때문에 @type이라는 이름의 필드가 자동으로 추가된다. 값은 클래스명이 된다
+        // property 요소의 값으로 필드명은 변경 가능하다
+        // include 요소의 기본 값은 PROPERTY이므로, JSON 객체 내에서 프라퍼티로 추가되었다
+
+        Car car = mapper.readValue(json, Car.class);
+        assertThat(car.getLicensePlate()).isEqualTo("68오 8269");
+
+        Vehicle vehicle2 = mapper.readValue(json, Vehicle.class);
+        assertThat(((Car) vehicle2).getLicensePlate()).isEqualTo("68오 8269");
+        // 타입 식별자가 존재하기 때문에 상위 타입으로 참조하더라도 문제 없음
+    }
+
+    @JsonTypeInfo(use = Id.NAME)
+    @JsonSubTypes({@Type(Car.class), @Type(Aeroplane.class)}) // 역직렬화 시 필요
+    static abstract class Vehicle {
+
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @Getter
+    static class Car extends Vehicle {
+
+        private String licensePlate;
+
+        public Car(String licensePlate) {
+            this.licensePlate = licensePlate;
+        }
+    }
+
+    static class Aeroplane extends Vehicle {
+
+        private int wingSpan;
+    }
+}
---
+++ src/test/java/learn/jackson/databind/polymorphism/NoTypeInfoTest.java
@@ -0,0 +1,59 @@
+package learn.jackson.databind.polymorphism;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+class NoTypeInfoTest {
+
+    ObjectMapper mapper = new ObjectMapper()
+        .enable(SerializationFeature.INDENT_OUTPUT);
+
+    @Test
+    void noTypeInfo() throws JsonProcessingException {
+        Vehicle vehicle = new Car("68오 8269");
+
+        String json = mapper.writeValueAsString(vehicle);
+        assertThat(json).isEqualTo("""
+                                       {
+                                         "licensePlate" : "68오 8269"
+                                       }""");
+
+        Car car = mapper.readValue(json, Car.class);
+        assertThat(car.getLicensePlate()).isEqualTo("68오 8269");
+
+        assertThatThrownBy(() -> mapper.readValue(json, Vehicle.class))
+            .isInstanceOf(InvalidDefinitionException.class);
+        // 아래 예외 발생
+        // Cannot construct instance of `learn.jackson.databind.polymorphism.NoTypeInfoTest$Vehicle` (no Creators, like default constructor, exist):
+        //  abstract types either need to be mapped to concrete types, have custom deserializer, or contain additional type information
+    }
+
+    static abstract class Vehicle {
+
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @Getter
+    static class Car extends Vehicle {
+
+        private String licensePlate;
+
+        public Car(String licensePlate) {
+            this.licensePlate = licensePlate;
+        }
+    }
+
+    static class Aeroplane extends Vehicle {
+
+        private int wingSpan;
+    }
+}
